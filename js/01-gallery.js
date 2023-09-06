c37f47f2cfdea12aa1ebfbe9b237ae0314446bf8
FlBorys <131404105+FlBorys@users.noreply.github.com>
2023-09-06 23:19:11 +0300

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const pictureContainer = document.querySelector('ul.gallery');
+const cardMarkup = createPictureCardsMarup(galleryItems);
+
+pictureContainer.insertAdjacentHTML('beforeend', cardMarkup);
+pictureContainer.addEventListener('click', choosePictureCard);
+window.addEventListener('keyup', closeModalWindow);
+
+function createPictureCardsMarup(elements) {
+    return elements.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    })
+        .join('');
+    };
+
+let lightBox;
+
+function choosePictureCard(evt) {
+    evt.preventDefault();
+    if (evt.target.tagName === 'IMG') {
+        lightBox = basicLightbox.create(`<img src="${evt.target.dataset.source}" alt="${evt.target.alt}">`);
+        lightBox.show();
+    };
+};
+
+function closeModalWindow(e) {
+    if (e.key === 'Escape') {
+        lightBox.close();
+    }
+}
+
+
+console.log(galleryItems);
